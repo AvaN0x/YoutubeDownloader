8f95cb2018fc9e3bd0f85cfba9a4f758a6332b8b
AvaN0x <clement.ricatte57@gmail.com>
2021-03-16 16:54:19 +0100

    add avancement on the project

---
+++ YoutubeDownloader/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace YoutubeDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Trace.WriteLine("text");
+            testVideoAsync("https://youtu.be/o-YBDTqX_ZU");
+            InitializeComponent();
+        }
+
+        public async Task testVideoAsync(String link)
+        {
+            var youtube = new YoutubeClient();
+
+            var video = await youtube.Videos.GetAsync(link);
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+
+            var streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
+
+            if (streamInfo != null && video != null)
+            {
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"C:\\Users\\cleme\\Downloads\\{video.Title}.mp3");
+                Trace.WriteLine("download done");
+            }
+            else
+                Trace.WriteLine("is null");
+        }
+
+        private void OnTop_Checked(object sender, RoutedEventArgs e)
+        {
+            Topmost = true;
+        }
+
+        private void OnTop_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Topmost = false;
+        }
+
+        private void txtbx_input_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
+
+                Trace.WriteLine("Start download");
+            }
+        }
+    }
+}
