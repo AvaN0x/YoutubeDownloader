ccb1dc634f64943194a54f57a7ba6495f64226f4
AvaN0x <clement.ricatte57@gmail.com>
2021-03-22 15:22:37 +0100

    now getting windows download folder by default

--- YoutubeDownloader/MainWindow.xaml.cs
+++ YoutubeDownloader/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Shapes;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 using YoutubeExplode.Exceptions;
+using Microsoft.Win32;
 
 namespace YoutubeDownloader
 {
@@ -30,7 +31,8 @@ namespace YoutubeDownloader
         {
             InitializeComponent();
 
-            txtbx_folder.Text = Directory.GetCurrentDirectory();
+            //txtbx_folder.Text = Directory.GetCurrentDirectory();
+            txtbx_folder.Text = (string?)Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", null) ?? Directory.GetCurrentDirectory();
         }
 
         private void OnTop_Checked(object sender, RoutedEventArgs e)
