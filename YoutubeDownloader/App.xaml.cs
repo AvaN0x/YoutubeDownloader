1b0cd9b12475ac732af06c6dfe04e2948e379d72
AvaN0x <clement.ricatte57@gmail.com>
2021-03-30 18:45:33 +0200

    now allowing only one instance of YoutubeDownloader

--- YoutubeDownloader/App.xaml.cs
+++ YoutubeDownloader/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,61 @@ namespace YoutubeDownloader
     /// </summary>
     public partial class App : Application
     {
+        private const string UniqueEventName = "{github.com/AvaN0x/YoutubeDownloader UniqueEventName}";
+        private const string UniqueMutexName = "{github.com/AvaN0x/YoutubeDownloader UniqueMutexName}";
+        private EventWaitHandle? _eventWaitHandle;
+        private Mutex? _mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Inspired from https://stackoverflow.com/a/23730146/12183781
+
+            // isNewInstance to check if an instance is already running
+            this._mutex = new Mutex(true, UniqueMutexName, out bool isNewInstance);
+            // Create an event or get the event of the other instance
+            this._eventWaitHandle = new EventWaitHandle(false, EventResetMode.AutoReset, UniqueEventName);
+
+            // So, R# would not give a warning that this variable is not used.
+            GC.KeepAlive(this._mutex);
+
+            // Program is already running
+            if (!isNewInstance)
+            {
+                // Trigger event of other instance
+                this._eventWaitHandle.Set();
+
+                this.Shutdown();
+                return;
+            }
+
+            // Create a thread that is waiting for the event
+            new Thread(() =>
+                {
+                    while (this._eventWaitHandle.WaitOne())
+                    {
+                        Current.Dispatcher.BeginInvoke((Action)(
+                            () =>
+                            {
+                                // We bring the window to foreground
+                                var mainWindow = (MainWindow)Current.MainWindow;
+                                if (mainWindow.WindowState == WindowState.Minimized || mainWindow.Visibility == Visibility.Hidden)
+                                {
+                                    mainWindow.Show();
+                                    mainWindow.WindowState = WindowState.Normal;
+                                }
+
+                                mainWindow.Activate();
+                                var oldTopMost = mainWindow.Topmost;
+                                mainWindow.Topmost = true;
+                                mainWindow.Topmost = oldTopMost;
+                                mainWindow.Focus();
+                            }
+                        ));
+                    }
+                })
+            {
+                IsBackground = true
+            }.Start();
+        }
     }
 }
